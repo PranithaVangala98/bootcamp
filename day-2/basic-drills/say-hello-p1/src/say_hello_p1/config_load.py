cd3e841b995f79e1cef620ae195b82e2daa1d6e7
Pranitha <pranithavanagala1998@gmai.com>
2025-05-09 09:31:16 +0530

    say hello and many hello

--- day-2/basic-drills/say-hello-p1/src/say_hello_p1/config_load.py
+++ day-2/basic-drills/say-hello-p1/src/say_hello_p1/config_load.py
@@ -4,11 +4,16 @@ import yaml
 from pathlib import Path
 from typing import Optional
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def _find_config_file() -> Optional[Path]:
     # 1. Check current directory
     current_dir = Path.cwd() / "_config.yaml"
     if current_dir.exists():
+        logger.info("using config from current directory")
         return current_dir
 
     # 2. Check CONFIG_PATH environment variable
@@ -16,9 +21,11 @@ def _find_config_file() -> Optional[Path]:
     for path in config_paths:
         config_path = Path(path) / "_config.yaml"
         if config_path.exists():
+            logger.info("using config from CONFIG_PATH env variable")
             return config_path
 
     # 3. Fallback to default shipped config
+    logger.info("using default config from library")
     return Path(__file__).parent / "default_config" / "_config.yaml"
 
 
